3fb3554dc47d130f9e98eba0ca940fc764e8375a
Pat East <that-pat@github.com>
2018-04-02 13:14:39 -0500

    Acknowledging old messages

--- echobot.py
+++ echobot.py
@@ -16,10 +16,18 @@ def get_json_from_url(url):
   js = json.loads(content)
   return js
 
-def get_updates():
+def get_updates(offset=None):
   url = URL + "getUpdates"
+  if offset:
+    url += "?offset={}".format(offset)
   js = get_json_from_url(url)
   return js
+
+def get_last_update_id(updates):
+  update_ids = []
+  for update in updates["result"]:
+    update_ids.append(int(update["update_id"]))
+  return max(update_ids)
 
 def get_last_chat_id_and_text(updates):
   num_updates = len(updates["result"])
@@ -32,14 +40,23 @@ def send_message(text, chat_id):
   url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
   get_url(url)
 
-def main():
-  last_textchat = (None, None)
-  while True:
-    text, chat = get_last_chat_id_and_text(get_updates())
-    if (text, chat) != last_textchat:
+def echo_all(updates):
+  for update in updates["result"]:
+    try:
+      text = update["message"]["text"]
+      chat = update["message"]["chat"]["id"]
       send_message(text, chat)
-      last_textchat = (text, chat)
-    time.sleep(0.5)
+    except Exception as e:
+      print(e)
+
+def main():
+  last_update_id = None
+  while True:
+    updates = get_updates(last_update_id)
+    if len(updates["result"]) > 0:
+      last_update_id = get_last_update_id(updates) + 1
+      echo_all(updates)
+    time.sleep(2)
 
 if __name__ == '__main__':
   main()
