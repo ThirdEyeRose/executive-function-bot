6f4888ae50a4ec2c46c7a713ba0d485853abe371
Pat East <that-pat@github.com>
2018-04-02 13:29:24 -0500

    Add URL quote encoding for body text

--- echobot.py
+++ echobot.py
@@ -1,6 +1,7 @@
 import time
 import json
 import requests
+import urllib
 
 from CREDENTIALS import *
 
@@ -30,6 +31,7 @@ def get_last_update_id(updates):
   return max(update_ids)
 
 def send_message(text, chat_id):
+  text = urllib.quote_plus(text)
   url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
   get_url(url)
 
