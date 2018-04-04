192112446f40a61691683973952004abad3c04c7
Pat East <that-pat@github.com>
2018-04-03 20:25:44 -0500

    First attempt at creating database tables for FT

--- dbhelper.py
+++ dbhelper.py
@@ -1,17 +1,20 @@
 import sqlite3
 
 class DBHelper:
-  def __init__(self, dbname="todo.sqlite"):
+  def __init__(self, dbname="efb.sqlite"):
     self.dbname = dbname
     self.conn = sqlite3.connect(dbname)
 
   def setup(self):
-    tblstmt = "CREATE TABLE IF NOT EXISTS items (description text, owner text)"
-    itemidx = "CREATE INDEX IF NOT EXISTS itemIndex ON items (description ASC)"
-    ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON items (owner ASC)"
-    self.conn.execute(tblstmt)
-    self.conn.execute(itemidx)
-    self.conn.execute(ownidx)
+    create_item_table = "CREATE TABLE IF NOT EXISTS items (description text, owner text)"
+    create_item_index = "CREATE INDEX IF NOT EXISTS itemIndex ON items (description ASC)"
+    create_owner_index = "CREATE INDEX IF NOT EXISTS ownIndex ON items (owner ASC)"
+    create_feelings_table = "CREATE TABLE IF NOT EXISTS feelings (created_at datetime, owner text, rating integer, description text)"
+    creat_feelings_config = "CREATE TABLE IF NOT EXISTS feelingsConfig (owner text, frequency text, time_pref text)"
+    self.conn.execute(create_item_table)
+    self.conn.execute(create_item_index)
+    self.conn.execute(create_owner_index)
+    self.conn.execute(create_feelings_table)
     self.conn.commit()
 
   def add_item(self, item_text, owner):
@@ -30,3 +33,18 @@ class DBHelper:
     stmt = "SELECT description FROM items WHERE owner = (?)"
     args = (owner, )
     return [x[0] for x in self.conn.execute(stmt, args)]
+
+  def add_feeling_report(self, created_at, owner, rating, description=None):
+    stmt = "INSERT INTO feelings (created_at, owner, rating, description) VALUES (?, ?, ?, ?)"
+    args = (created_at, owner, rating, description)
+    self.conn.execute(stmt, args)
+    self.conn.commit()
+
+  def get_feeling_report(self, owner):
+    stmt = "SELECT rating FROM feelings WHERE owner = (?)"
+
+  def set_feelings_config(self, owner, frequency, time_pref):
+    stmt = "INSERT INTO feelingsConfig (owner, frequency, time_pref) VALUES (?, ?)"
+    args = (owner, frequency, time_pref)
+    self.conn.execute(stmt, args)
+    self.conn.commit()
